d8d3fbf8fb0e037fa976b316a8cf180529ffe06b
21seu <1693376841@qq.com>
2021-07-30 22:23:29 +0800

    异常处理

---
+++ src/main/java/com/ftj/handler/ControllerExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.ftj.handler;
+
+/**
+ * Created by fengtj on 2021/7/30 21:09
+ */
+public class ControllerExceptionHandler {
+}
---
+++ src/main/java/com/ftj/web/IndexController.java
@@ -0,0 +1,7 @@
+package com.ftj.web;
+
+/**
+ * Created by fengtj on 2021/7/30 21:03
+ */
+public class IndexController {
+}

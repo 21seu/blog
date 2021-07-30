59d65795dfb46b352a420be8b17da4d1a2f15415
21seu <1693376841@qq.com>
2021-07-30 22:24:26 +0800

    异常处理

--- src/main/java/com/ftj/handler/ControllerExceptionHandler.java
+++ src/main/java/com/ftj/handler/ControllerExceptionHandler.java
@@ -1,7 +1,42 @@
 package com.ftj.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by fengtj on 2021/7/30 21:09
  */
+@ControllerAdvice
 public class ControllerExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    /**
+     * 同意拦截的所有exception
+     *
+     * @param request
+     * @param e
+     * @return
+     */
+    @ExceptionHandler({Exception.class})
+    public ModelAndView exceptionHandler(HttpServletRequest request, Exception e) throws Exception {
+        logger.error("Request URL : {}, Exception : {}", request.getRequestURL(), e);
+
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("url", request.getRequestURL());
+        mv.addObject("exception", e);
+        mv.setViewName("error/error");
+        return mv;
+    }
 }
--- src/main/java/com/ftj/web/IndexController.java
+++ src/main/java/com/ftj/web/IndexController.java
@@ -1,7 +1,17 @@
 package com.ftj.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by fengtj on 2021/7/30 21:03
  */
+@Controller
 public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        int i = 9 / 0;
+        return "index";
+    }
 }

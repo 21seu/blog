23137bc588d7c18b2d906f75fdf48a21cad4e7dc
21seu <1693376841@qq.com>
2021-07-31 16:33:42 +0800

    日志处理

---
+++ src/main/java/com/ftj/aspect/LogAspect.java
@@ -0,0 +1,76 @@
+package com.ftj.aspect;
+
+import lombok.Data;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * Created by fengtj on 2021/7/31 16:05
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Pointcut("execution(* com.ftj.web.*.*(..) )") //【修饰符】(修饰符可以省略)  返回值  包  类  方法(参数)
+    public void log() {
+
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        //localhost会是ipv6地址
+        String ip = request.getRemoteAddr();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog(url, ip, classMethod, args);
+        logger.info("Request : {}", requestLog);
+    }
+
+    @After("log()")
+    public void doAfter() {
+        logger.info("--------doAfter--------");
+    }
+
+    @AfterReturning(returning = "result", pointcut = "log()")
+    public void doAfterReturn(Object result) {
+        logger.info("Result : {}", result);
+    }
+
+    private class RequestLog {
+        private String url;
+        private String ip;
+        private String classMethod;
+        private Object[] args;
+
+        public RequestLog(String url, String ip, String classMethod, Object[] args) {
+            this.url = url;
+            this.ip = ip;
+            this.classMethod = classMethod;
+            this.args = args;
+        }
+
+        @Override
+        public String toString() {
+            return "RequestLog{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    '}';
+        }
+    }
+}
--- src/main/java/com/ftj/web/IndexController.java
+++ src/main/java/com/ftj/web/IndexController.java
@@ -2,6 +2,7 @@ package com.ftj.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by fengtj on 2021/7/30 21:03
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        int i = 9 / 0;
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+
+        System.out.println("--------index--------");
         return "index";
     }
 }

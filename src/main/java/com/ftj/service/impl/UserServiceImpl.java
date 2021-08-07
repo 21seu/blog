1785963f0421c6c31e1e30c6f5d67ea0bde6802a
21seu <1693376841@qq.com>
2021-08-07 16:26:38 +0800

    MD5加密

--- src/main/java/com/ftj/service/impl/UserServiceImpl.java
+++ src/main/java/com/ftj/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.ftj.service.impl;
 import com.ftj.dao.UserRepository;
 import com.ftj.pojo.User;
 import com.ftj.service.UserService;
+import com.ftj.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 
